f2ba95d4019f3d0f882276bc95e7b2f81ee991a5
Artem <Artemrid@yandex.ru>
2022-06-18 18:16:41 +0300

    Course_2_HomeWork_1

---
+++ Ex_1/Ex_1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SimpleNumber(3);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                SimpleNumber(5);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                SimpleNumber(7);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                SimpleNumber(49);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                SimpleNumber(-50);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey(true);
+        }
+        static void SimpleNumber(int n)
+        {
+            if (n<0)
+            {
+                throw new Exception("Число должно быть больше 0");
+            }
+            int d = 0;
+            int i = 2;
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            if (d == 0)
+            {
+                Console.WriteLine($"{n} - число простое");
+            }
+            else
+            {
+                Console.WriteLine($"{n} - число сложное");
+            }
+        }
+    }
+}
---
+++ Ex_3/Ex_3/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(Fibbo(10));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                Console.WriteLine(Fibbo(1));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                Console.WriteLine(Fibbo(2));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                Console.WriteLine(Fibbo(30));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                Console.WriteLine(Fibbo(-25));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey(true);
+        }
+        /// <summary>
+        /// Метод через рекурсию
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        static int FibboRec(int n)
+        {
+            if (n < 0)
+            {
+                throw new Exception("Число должно быть больше 0");
+            }
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            return FibboRec(n - 1) + FibboRec(n - 2);
+        }
+        /// <summary>
+        /// Метод через цикл без рекурсии
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        static int Fibbo(int n)
+        {
+            if(n<0)
+            {
+                throw new Exception("Число должно быть больше 0");
+            }
+            int number = n;
+            if(n==1)
+            {
+                Console.Write($"Число фиббоначи номер {number} = ");
+                return n;
+            }
+            int frst = 0;
+            int scnd = 1;
+            int summa=0;
+            while (n > 1)
+            {
+                summa = frst + scnd;
+                frst = scnd;
+                scnd = summa;
+                n = n - 1;
+            }
+            Console.Write($"Число фиббоначи номер {number} = ");
+            return summa;
+
+        }
+    }
+}
